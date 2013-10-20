f3ed2e5d716fda140ab6265b4de452df158dd1c3
taraniselsu <taraniselsu@gmail.com>
2013-10-20 13:02:19 -0700

    Fleshed out the right-click event example.

--- 03-PartRightClicking/Source/PartRightClick.cs
+++ 03-PartRightClicking/Source/PartRightClick.cs
@@ -24,50 +24,93 @@ using UnityEngine;
 namespace Tac
 {
     /*
-     * This project is meant to demonstrate how to make a part with right-click options.
+     * This project is meant to demonstrate how to make a part with right-click events.
      * 
      * Please see the SimplePartModule example first. This class will not explain anything not
-     * pertaining to making right-click options.
+     * pertaining to making right-click events.
      * 
      * To make something show up on the right click menu, create a method with the KSPEvent attribute.
      * 
-     * Open the Debug console when running this example. On Windows, you press Alt+F2 while in game.
-     * For other OS'es, use the appropriate modifier and F2.
+     * KSPEvent has the following parameters:
+     *    active = Controls whether the event is active (or enabled). The default value is true.
+     *    guiName = The name to show for the event. The default value is an empty string, which will
+     *       make KSP use the method name instead.
+     *    guiActive = Controls whether the event is active while controlling the vessel with the
+     *       part. The default value is false.
+     *    guiActiveUnfocused = Controls whether the event is active while controlling a nearby
+     *       vessel. The default value is false.
+     *    externalToEVAOnly = Only has an effect if guiActiveUnfocused is true, then it controls
+     *       whether the event is active for nearby vessels or only for nearby EVAs. The default
+     *       value is true, which means that the event is only active for nearby EVAs and not
+     *       for nearby vessels.
+     *    unfocusedRange = The maximum range at which the event can be triggered while controlling
+     *       a different vessel or an EVA. The default value is 
+     *    name = Unknown
+     *    category = Unknown
+     *    isDefault = Unknown
+     *    guiIcon = Unknown
+     *    
+     * If you want an event to be active for:
+     *   the vessel with the part     set guiActive to true
+     *   a nearby Kerbal on EVA       set guiActiveUnfocused to true
+     *   a nearby vessel              set guiActiveUnfocused to true, and externalToEVAOnly to false
      */
     public class PartRightClick : PartModule
     {
         /*
-         * This first event is almost as simple as you can get. We want it to show on the menu, so
-         * we set a guiName and set guiActive to true.
+         * This event is active when controlling the vessel with the part.
          */
-        [KSPEvent(guiActive = true, guiName = "Event 1")]
-        public void Event1()
+        [KSPEvent(guiActive = true, guiName = "Activate")]
+        public void ActivateEvent()
         {
-            Debug.Log("TAC Examples-PartRightClick [" + this.GetInstanceID().ToString("X")
-                + "][" + Time.time.ToString("0.0000") + "]: Event1");
-            ScreenMessages.PostScreenMessage("Clicked Event 1", 10.0f, ScreenMessageStyle.UPPER_CENTER);
+            ScreenMessages.PostScreenMessage("Clicked Activate", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+
+            // This will hide the Activate event, and show the Deactivate event.
+            Events["ActivateEvent"].active = false;
+            Events["DeactivateEvent"].active = true;
         }
 
         /*
-         * This event is clickable when the part is not on the active ship, i.e. from a near-by ship or EVA.
+         * This event is also active when controlling the vessel with the part. It starts disabled.
          */
-        [KSPEvent(guiActive = true, guiName = "Event 2", guiActiveUnfocused = true, unfocusedRange = 100.0f)]
-        public void Event2()
+        [KSPEvent(guiActive = true, guiName = "Deactivate", active = false)]
+        public void DeactivateEvent()
         {
-            Debug.Log("TAC Examples-PartRightClick [" + this.GetInstanceID().ToString("X")
-                + "][" + Time.time.ToString("0.0000") + "]: Event2");
-            ScreenMessages.PostScreenMessage("Clicked Event 2", 10.0f, ScreenMessageStyle.UPPER_CENTER);
+            ScreenMessages.PostScreenMessage("Clicked Deactivate", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+
+            // This will hide the Deactivate event, and show the Activate event.
+            Events["ActivateEvent"].active = true;
+            Events["DeactivateEvent"].active = false;
         }
 
         /*
-         * externalToEVAOnly makes it only work while on EVA.
+         * This event is active when controlling a nearby EVA. It can be activated from up to 25
+         * meters away.
          */
-        [KSPEvent(guiActive = true, guiName = "Event 3", guiActiveUnfocused = true, unfocusedRange = 100.0f, externalToEVAOnly = false)]
-        public void Event3()
+        [KSPEvent(guiActiveUnfocused = true, unfocusedRange = 25f, guiName = "Nearby EVA")]
+        public void NearbyEvaEvent()
         {
-            Debug.Log("TAC Examples-PartRightClick [" + this.GetInstanceID().ToString("X")
-                + "][" + Time.time.ToString("0.0000") + "]: Event3");
-            ScreenMessages.PostScreenMessage("Clicked Event 3", 10.0f, ScreenMessageStyle.UPPER_CENTER);
+            ScreenMessages.PostScreenMessage("Clicked NearbyEvaEvent", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+        }
+
+        /*
+         * This event is active when controlling a nearby vessel. Note that the event will also be
+         * active for nearby EVAs. There is not currently a way to make it only active for nearby
+         * vessels.
+         */
+        [KSPEvent(guiActiveUnfocused = true, externalToEVAOnly = false, unfocusedRange = 25f, guiName = "Nearby Vessel")]
+        public void NearbyVesselEvent()
+        {
+            ScreenMessages.PostScreenMessage("Clicked NearbyVesselEvent", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+        }
+
+        /*
+         * This event is always active, regardless of which vessel is being controlled.
+         */
+        [KSPEvent(guiActive = true, guiActiveUnfocused = true, externalToEVAOnly = false, unfocusedRange = 25f, guiName = "Any Event")]
+        public void AnyEvent()
+        {
+            ScreenMessages.PostScreenMessage("Clicked AnyEvent", 5.0f, ScreenMessageStyle.UPPER_CENTER);
         }
     }
 }
